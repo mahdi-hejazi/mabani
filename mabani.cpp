4c37df96d6c2d4e3961c8804da90e2381c9fe440
mahdi <you@example.com>
2018-12-31 15:40:06 +0330

    main source code of project

---
+++ mabani.cpp
@@ -0,0 +1,155 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+#include <fstream>
+#include<string.h>
+using namespace std;
+/*file haie ma shamel : accounts baraie account ha
+customer baraie atelaat moshtary ha
+drivers baraie ranande ha
+nessesary baraie atelaat digar mesle leader dar khat aval*/
+struct date
+{
+  int day;
+  int month;
+  int year;
+}
+struct history   //struct history is for history of travels
+
+{
+  int number;
+  char bigining[30];
+  char distination[30];
+  int  price;
+  struct date d;
+
+}
+struct leader //first pass=a      firs user=a
+{
+	char pass[30];
+	char username[30];
+}
+struct driver
+{
+	char username[30];
+	char password[30];
+	struct accont a;
+	struct history h[10000];
+}
+struct account
+{
+	int balance;
+	int pass;
+	int inventory;//mojodi
+}
+struct customer
+{
+  char username[30];
+  char name[30];
+  char family[30];
+  
+  char password[30];
+  char phone[15];
+  
+  int moaref;
+  struct account;
+  struct history h[1000];
+  
+}
+void edit-karbar()
+{
+	ifstream f1("customer",ios :: binary);
+    if (!f1)
+     {
+    	cout<<"error in opening customer file";
+    	exit(1);
+     }
+  
+}
+void sign-up()
+{
+	
+}
+int log-in()
+{
+	char username[30];
+	char pass[30];
+	int position;
+	cout<<"enter the number of your position and press enter:\n 1 leder \t 2 driver \t 3 customer \t 4 no psition \n;
+	cin>>positin;
+	switch(psition)
+	{
+		case 1 {
+			struct leader l1;
+			char user[30],pass[30];
+			ifstream f1("nessesary", ios :: binary);
+			if(!f1)
+		    	{
+			        cout<<"error in opening file nessesary in log-in";
+			        exit(1);
+		    	}
+		    while(1)
+			{
+				
+				cout<<"enter your username: \t";
+		        scanf("%S",user);
+		        f1.read((char *)l1,sizeof(struct leader));
+		        if(strcmp(user,l1.username)>0)
+		        {
+		        	for(int i=0;i<3;i++)
+		        	{
+		        		scanf<<"enter your password : \t";
+		        	    scanf("%s",pass);
+		          	    if(strcmp(pass,l1.pass)>0)
+					    {
+						    cout<<"\n you log in as a leader : \t"
+					        f1.close();
+					    }
+					    else
+					    {
+					    	cout<<"\n your password is false \n try again \n";
+					    	
+						}
+					}
+					cout<<"you tried several time";
+					exit(0);
+		        	
+				}
+				else
+				{
+					cout << " your user name is false. \n do you want to enter another one?(y/n)";
+					char s;
+					cin >> s;
+					if(s==y || s==Y) continue;
+					if(s==n || s==N) 
+					{
+						cout<< "good buy";
+						exit(0);
+					}
+				}
+			}	
+	       
+		    
+			   }
+		case 2
+			   {
+			   	   	
+			   }
+	
+	}
+}
+int main()
+{
+	struct customer[10000];//hadaksar 100000 moshtary darim be andaze hesab ha
+    
+    ifstream f2("accounts.txt");
+    if(!f2)
+     {
+    	cout<<"error in opening account file";
+    	exit(1);
+     }
+  
+  
+  
+  
+}
