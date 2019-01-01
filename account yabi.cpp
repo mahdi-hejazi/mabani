1dc6988eb12d32fea973f5b2bb136f5124ab5cbc
mahdi <you@example.com>
2019-01-01 10:32:55 +0330

    add some soruse code forexample account yabi

---
+++ account yabi.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct account
+{
+	int balance;
+	int pass;
+	int inventory;//mojodi
+};
+int main()
+{
+	struct account a,b; //a baraye gereftan az file  va b baraie gereftan az karbar
+	ifstream f1("accounts", ios :: binary);
+	if(!f1)
+	 {
+	   cout<<"error in opening accounts binary file";
+	   exit(1);
+	 }
+	 while(1)
+	{
+		 cout<<"enter your account :\t";
+	     cin>>b.balance;
+         while(!f1.eof())
+	 {
+	 	f1.read((char *)&a,sizeof(struct account));
+	 	if(a.balance == b.balance)
+	 	 {
+	 		
+	 		for(int i=0;i<3;i++)
+	 		{
+	 			cout<<"\n enter your password:\t";
+	 			cin>>b.pass;
+	 			if(b.pass == a.pass)
+	 			{
+	 				cout<<"\nyour pass was right\nyour inventory is :\t"<<a.inventory;
+	 				return 1;//1 bargardine yani account mojode
+				}
+				else cout<<"your password was false\n you can try "<<2-i<<"times"<<endl;
+			 }
+	 		
+		 }
+	 }
+	 char i;
+	 cout <<"your account is false.\n do you wanna try again?(y/n)\n";
+	 cin>>i;
+	 if(i=='n') 
+	 {
+	 	cout <<"good bye\n";
+	 	return 0;
+	 }
+	 
+	}
+}
